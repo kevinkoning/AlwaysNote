1ddf081c6b61068e80d724676ae66cfb829c4b29
Kevin <kevinkoning99@gmail.com>
2026-02-12 14:56:25 -0800

    Initial Commit

---
+++ AlwaysNote/AlwaysNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysNoteApp.swift
+//  AlwaysNote
+//
+//  Created by Kief on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
